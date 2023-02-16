c02bba914a82a07ce39dd968a32557e26370cfc7
jmj073 <wjsalswns0733@gmail.com>
2023-02-16 22:02:09 +0900

    first commit

---
+++ ps/BOJ_4101.sh
@@ -0,0 +1,20 @@
+# 두수 크기 비교
+
+# 1
+# while read a b; ((a + b));
+# do
+# 	if ((a > b))
+# 	then
+# 		echo Yes
+# 	else
+# 		echo No
+# 	fi
+# done
+
+# 2
+# while read a b; ((a + b));
+# do
+# 	((a > b)) && echo Yes || echo No
+# done
+
+#3
